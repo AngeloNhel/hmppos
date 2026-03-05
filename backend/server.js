7d03b4239458d147b27b24528cf05d640a07a19a
Angelo Acibar <gelogigs14@gmail.com>
2026-03-05 17:37:05 +0800

    Fix login API to use deployed backend

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,23 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+// allow requests from your Netlify frontend
+app.use(cors({
+  origin: "https://brilliant-concha-645eab.netlify.app",
+}));
+
 app.use(express.json());
 
+// routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/sales", require("./routes/salesRoutes"));
 app.use("/api/sale-items", require("./routes/saleItemsRoute"));
 app.use("/api/stock-history", require("./routes/stockHistoryRoutes"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+// use dynamic port for deployment
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
